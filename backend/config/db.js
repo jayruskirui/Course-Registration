7445408f28dbdb47b0dd7f5a3a6ccdec27b0432d
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-09-03 23:41:09 +0300

    updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,10 +7,13 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // RDS uses a cert chain that Node doesn't trust by default
+  },
 });
 
 pool.on("connect", () => {
-  console.log("Connected to PostgreSQL database");
+  console.log("Connected to PostgreSQL database (RDS)");
 });
 
 pool.on("error", (err) => {
